43a1c6058e27cb242f15cefe83dfc533709e470e
Aminul Islam <aminultension@gmail.com>
2024-09-24 01:20:38 +0530

    add adminMiddleware

--- server-mess/routers/admin.js
+++ server-mess/routers/admin.js
@@ -2,12 +2,12 @@ const express = require('express');
 const adminController = require('../controller/admin-controllers');
 const router = express.Router();
 
-const authMiddleware = require('../middleware/auth');
+const adminMiddleware = require('../middleware/admin');
 
 router.route('/').get(adminController.home);
-router.route('/pending-users').get(authMiddleware, adminController.getPendingUsers);
-router.route('/approve-user/:id').put(authMiddleware, adminController.approveUser);
-router.route('/deny-user/:id').put(authMiddleware, adminController.denyUser);
-router.route('/approved-users').get(authMiddleware, adminController.getApprovedUsers);
+router.route('/pending-users').get(adminMiddleware, adminController.getPendingUsers);
+router.route('/approve-user/:id').put(adminMiddleware, adminController.approveUser);
+router.route('/deny-user/:id').put(adminMiddleware, adminController.denyUser);
+router.route('/approved-users').get(adminMiddleware, adminController.getApprovedUsers);
 
 module.exports = router;

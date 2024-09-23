346f9c576c2f5e4cfbd0483cf8d9e845c0efe9ef
Aminul Islam <aminultension@gmail.com>
2024-09-23 22:13:13 +0530

    modify the user Schema

--- server-mess/models/User.js
+++ server-mess/models/User.js
@@ -1,39 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     phoneNumber: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     verificationCode: {
-        type: String,
-        required: false,
+      type: String,
+      required: false,
     },
     isVerified: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     isPending: {
-        type: Boolean,
-        default: true,
+      type: Boolean,
+      default: true,
     },
     isApproved: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-}, { timestamps: true });
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.methods = {
+  remove: function () {
+    // Notice the capital 'R'
+    console.log(`Removing user: ${this._id}`);
+    return this.deleteOne();
+  },
+};
+
+module.exports = mongoose.model("User", userSchema);
